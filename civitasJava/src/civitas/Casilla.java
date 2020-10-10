4eed5e18abf6d30fac6306fe07904a8161eca1ff
Esther García <esther.g.g.01@gmail.com>
2020-10-10 17:52:14 +0200

    Comentarios doxygen en clase Casilla

--- civitasJava/src/civitas/Casilla.java
+++ civitasJava/src/civitas/Casilla.java
@@ -1,19 +1,30 @@
-package civitas;
-
 /**
  * @author Yesenia González Dávila
  * @author Esther García Gallego
  * Grupo B.3
  */
 
+package civitas;
+
+/**
+ * @class Casilla
+ * @brief Crea las casillas del tablero
+ * @warning Not fully implemented. Clase temporal
+*/
 public class Casilla {
-    
+
     private String nombre;
-    
+
+    /*
+    * @brief Constructor de la clase casilla
+    */
     Casilla (String n) {
         nombre = n;
     }
-    
+
+    /*
+    * @brief Consultor del atributo getNombre
+    */
     String getNombre () {
         return nombre;
     }

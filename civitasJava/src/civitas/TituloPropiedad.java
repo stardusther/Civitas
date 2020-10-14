c832e90c2c24cd73f5ca50126c098d6f9500345b
ygonz <ygonz@correo.ugr.es>
2020-10-14 11:05:55 +0200

    Tit propiedad actualizado

--- civitasJava/src/civitas/TituloPropiedad.java
+++ civitasJava/src/civitas/TituloPropiedad.java
@@ -1,5 +1,11 @@
-package civitas;
+/**
+ * @file MazoSopresas.java
+ * @author Yesenia González Dávila
+ * @author Esther García Gallego
+ * @note Grupo B.3
+*/
 
+package civitas;
 public class TituloPropiedad {
     
     private String nombre;
@@ -16,6 +22,9 @@ public class TituloPropiedad {
     private int numCasas;
     private int numHoteles;
     
+    // Valor 'nulo' por defecto?
+    private Jugador propietario;
+    
     // Todos los titulos de propiedad comienzan existiendo sin propietario, casas ni hoteles y sin hipotecar
     TituloPropiedad (String _nombre, float base_alq, float factor_rev, float base_hip,
                      float precio_compra, float precio_edificar) {
@@ -29,23 +38,33 @@ public class TituloPropiedad {
         hipotecado = false;
         numCasas = 0;
         numHoteles = 0;
-        factorInteresesHipoteca = (float) 1.1 ;
+        factorInteresesHipoteca = 1.1f ;
     }
     
     void ActualizaPropietarioPorConversion (Jugador jugador) {
         
     }
     
+    /**
+     * @warning Pendiente
+     * @param jugador
+     * @return 
+     */
     boolean cancelarHipoteca (Jugador jugador) {
-        
+        return true;
     }
     
     int cantidadCasasHoteles() {
-        
+        return (numCasas + numHoteles);
     }
     
+    /**
+     * @warning Pendiente
+     * @param jugador
+     * @return 
+     */
     boolean comprar (Jugador jugador) {
-        
+        return true;
     }
     
     boolean construirCasa (Jugador jugador) {
@@ -65,31 +84,42 @@ public class TituloPropiedad {
     }
     
     boolean getHipotecado () {
-        
+        return hipotecado;
     }
     
     float getImporteCancelarHipoteca () {
-        
+        return (factorInteresesHipoteca * hipotecaBase);
     }
     
     float getImporteHipoteca () {
-        
+        return hipotecaBase;
     }
     
     String getNombre() {
-        
+        return nombre;
     }
     
     int getNumCasas() {
-        
+        return numCasas;
     }
     
     int getNumHoteles() {
-        
+        return numHoteles;
     }
     
     float getPrecioAlquiler() {
+        float precio;
+        //devuelve el precio del alquiler calculado según las reglas del
+        //juego. Si el título se encuentra hipotecado o si el propietario está encarcelado (ver
+        //propietarioEncarcelado()) el precio del alquiler será cero.
         
+        if (hipotecado || propietarioEncarcelado())
+            precio = 0f;
+        else
+            precio = 1f;
+            //Calcular precio (1 para que no de error de compilacion de momento)
+            
+        return precio;
     }
     
     float getPrecioCompra () {
@@ -101,11 +131,11 @@ public class TituloPropiedad {
     }
     
     float getPrecioVenta () {
-        
+        return (precioCompra + precioEdificar * (numCasas+numHoteles) * factorRevalorizacion);
     }
     
     Jugador getPropietario () {
-        
+        return propietario;
     }
     
     boolean hipotecar (Jugador jugador) {
@@ -124,8 +154,22 @@ public class TituloPropiedad {
         
     }
     
-    String toString() {
+    @Override
+    public String toString() {
+        String estado = " Estado: \n"
+                        + " >> Nombre: " + nombre + "\n"
+                        + " >> Alquiler base: " + alquilerBase + "\n"
+                        + " >> Hipoteca base: " + hipotecaBase + "\n"
+                        + " >> Factor de revalorizacion: " + factorRevalorizacion + "\n"
+                        + " >> Factor intereses hipoteca: " + factorInteresesHipoteca + "\n"
+                        +  ">> Factor intereses hipoteca: " + factorInteresesHipoteca + "\n"
+                        + " >> Precio compra: " + precioCompra + "\n"
+                        + " >> Precio edificar: " + precioEdificar + "\n"
+                        + " >> Hipotecado: " + hipotecado + "\n"
+                        + " >> Nº de casas: " + numCasas + "\n"
+                        + " >> Nº de hoteles: " + numHoteles + "\n" ;
         
+        return estado;
     }
     
     void tramitarAlquiler (Jugador jugador) {

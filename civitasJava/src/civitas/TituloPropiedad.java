f102d9e5f333dc30339b726eb4beb93531656523
ygonz <ygonz@correo.ugr.es>
2020-10-14 10:33:10 +0200

    TituloPropiedad actualizado

--- civitasJava/src/civitas/TituloPropiedad.java
+++ civitasJava/src/civitas/TituloPropiedad.java
@@ -1,14 +1,66 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package civitas;
 
-/**
- *
- * @author yese
- */
 public class TituloPropiedad {
+    
+    private String nombre;
+    
+    private float alquilerBase;
+    private float hipotecaBase;
+    private float factorRevalorizacion;
+    private float factorInteresesHipoteca;
+    private float precioCompra;
+    private float precioEdificar;
+    
+    private boolean hipotecado;
+    
+    private int numCasas;
+    private int numHoteles;
+    
+    // Todos los titulos de propiedad comienzan existiendo sin propietario, casas ni hoteles y sin hipotecar
+    TituloPropiedad (String _nombre, float base_alq, float factor_rev, float base_hip,
+                     float precio_compra, float precio_edificar) {
+        nombre = _nombre;
+        alquilerBase = base_alq;
+        factorRevalorizacion = factor_rev;
+        hipotecaBase = base_hip;
+        precioCompra = precio_compra;
+        precioEdificar = precio_edificar;
+        
+        hipotecado = false;
+        numCasas = 0;
+        numHoteles = 0;
+        factorInteresesHipoteca = (float) 1.1 ;
+    }
+    
+    void ActualizaPropietarioPorConversion (Jugador jugador) {
+        
+    }
+    
+    boolean cancelarHipoteca (Jugador jugador) {
+        
+    }
+    
+    int cantidadCasasHoteles() {
+        
+    }
+    
+    boolean comprar (Jugador jugador) {
+        
+    }
+    
+    boolean construirCasa (Jugador jugador) {
+        
+    }
+    
+    boolean construirHotel (Jugador jugador) {
+        
+    }
+    
+    boolean derruirCasas (int n, Jugador jugador) {
+        
+    }
+    
+    
+    
     
 }

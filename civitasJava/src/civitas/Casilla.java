93644bfca7f17ed653f8ad6c6bf59c3945c24318
ygonz <ygonz@correo.ugr.es>
2020-10-18 14:19:23 +0200

    Clase Casilla terminada

--- civitasJava/src/civitas/Casilla.java
+++ civitasJava/src/civitas/Casilla.java
@@ -9,14 +9,141 @@ package civitas;
  * @note Grupo B.3
 */
 public class Casilla {
-
+    
+    private TipoCasilla tipo;                       // Tipo de casilla
     private String nombre;
+    
+    private int carcel;                             // Juez
+    private float importe;                          // Impuesto
+    private Sorpresa sorpresa;                      // Sopresa
+    private MazoSorpresas mazo;                     // Mazo si la casilla es una sorpresa
+    private TituloPropiedad tituloPropiedad;        // Calle
 
-    Casilla (String n) {
-        nombre = n;
+    
+    /** Constructor casilla descanso. */ 
+    Casilla (String nombre) {
+        init();
+        this.nombre = nombre;
+        tipo = TipoCasilla.DESCANSO;
+    }
+    
+    /** Constructor casilla calle. */
+    Casilla (TituloPropiedad titulo) {
+        init();
+        nombre = titulo.getNombre();
+        tituloPropiedad = titulo;
+    }
+    
+    /** Constructor casilla impuesto. */
+    Casilla (float cantidad, String nombre) {
+        init();
+        this.nombre = nombre;
+        importe = cantidad;
+        tipo = TipoCasilla.CALLE;
+    }
+    
+    /** Constructor casilla juez. */
+    Casilla (int numCasillaCarcel, String nombre) {
+        init();
+        this.nombre = nombre;
+        carcel = numCasillaCarcel;
+        tipo = TipoCasilla.JUEZ;
+    }
+    
+    /** Constructor casilla sorpresa. */
+    Casilla (MazoSorpresas mazo, String nombre) {
+        init();
+        this.nombre = nombre;
+        this.mazo = mazo;
+        tipo = TipoCasilla.SORPRESA;
     }
 
-    String getNombre () {
+    
+    public boolean jugadorCorrecto (int actual, Jugador todos[]) {
+        return (actual < todos.length);
+    }
+    
+    
+    /** @warning Siguiente práctica */
+    void recibeJugador (int iactual, Jugador todos[]) {
+        
+    }
+    
+    
+    // ------------- Consultores -------------- //
+    
+    
+    public String getNombre () {
         return nombre;
+    }
+    
+    
+    TituloPropiedad getTituloPropiedad() {
+        return tituloPropiedad;
+    }
+    
+    
+    @Override
+    public String toString() {
+        String str = " >> Casilla " + nombre;
+        //Falta poner el resto de valores según el tipo de casilla (importe etc)
+       return str;
+    }
+    
+    
+    // ------------- Met. privados -------------- //
+    
+    
+    private void informe (int actual, Jugador todos[]) {
+        String str = "El jugador " + todos[actual] + " ha caido en una casilla.\n" + toString();
+        Diario.getInstance().ocurreEvento(str);
+    }
+    
+    
+    /** @warning Siguiente práctica */
+    private void recibeJugador_calle (int actual, Jugador todos[]) {
+        if (jugadorCorrecto (actual, todos)) {
+            informe (actual, todos);
+            todos[actual].pagaAlquiler(tituloPropiedad.getPrecioAlquiler());    // Y: creo?
+        }
+    }
+    
+    
+    private void recibeJugador_impuesto (int actual, Jugador todos[]) {
+        if (jugadorCorrecto (actual, todos)) {
+            informe (actual, todos);
+            todos[actual].pagaImpuesto(importe);
+        }
+    }
+    
+    
+    private void recibeJugador_juez (int actual, Jugador todos[]) {
+        if (jugadorCorrecto (actual, todos)) {
+            informe (actual, todos);
+            todos[actual].encarcelar(carcel);
+        }
+    }
+    
+    
+    /** @warning Siguiente práctica */
+    private void recibeJugador_sorpresa (int actual, Jugador todos[]) {
+        if (jugadorCorrecto (actual, todos)) {
+            informe (actual, todos);
+            sorpresa.aplicarAJugador(actual, todos);
+        }
+    }
+    
+    
+    /** Este metodo hace una inicializacion de todos los atributos asuminedo que no se proporciona
+     *  al constructor un valor para ese atributo. */
+    private void init () {
+        mazo = null;
+        sorpresa = null;
+        tituloPropiedad = null;
+        
+        importe = -1f;
+        carcel = -1;
+        
+        nombre = "";
     }
 }

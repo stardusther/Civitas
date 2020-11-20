c35c1b0c530b1dd4a1f2725d090255569b182c01
ygonz <ygonz@correo.ugr.es>
2020-11-20 17:32:42 +0100

    TitPropiedad.rb

--- civitasRuby/lib/civitas/TituloPropiedad.rb
+++ civitasRuby/lib/civitas/TituloPropiedad.rb
@@ -5,5 +5,35 @@ Authors: Esther García Gallego
 =end
 
 class TituloPropiedad  
+  
+  @nombre
+  
+  @alquilerBase
+  @hipotecaBase
+  @factorRevalorizacion
+  @precioCompra
+  @precioEdificar
+  
+  @hipotecado
+  
+  @numCasas
+  @numHoteles
+  
+  @propietario
+  
+  def initialize (nombre, ab, fr, hb, pc, pe)
+    @nombre = nombre
+    @alquilerBase = ab
+    @hipotecaBase = hb
+    @factorRevalorizacion = fr
+    @precioCompra = pc
+    @precioEdificar = pe
+    
+    @hipotecado = false
+    @propietario = nil
+    @numCasas = 0
+    @numHoteles = 0
+    @factorInteresesHipoteca = 1.1
+  end
 
 end

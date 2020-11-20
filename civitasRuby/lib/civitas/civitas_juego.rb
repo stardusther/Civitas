0f1f8186bfd5db771b6fbd5709dbe31973d3ee29
Esther García <esther.g.g.01@gmail.com>
2020-11-20 20:04:08 +0100

    Clase civitasJuego.rb terminada
    Falta inicializar los titulos propiedad, que falla el i++ (no existe en ruby)

--- civitasRuby/lib/civitas/civitas_juego.rb
+++ civitasRuby/lib/civitas/civitas_juego.rb
@@ -14,9 +14,9 @@ require_relative 'TituloPropiedad'
 module Civitas
   class CivitasJuego
     
-    @@NumJugadores = 2  # Todo lo que empiece con mayúsculas se considera constante
-    @@CasillaCarcel = 3 # Preguntar de todas formas
-    @@NumCasillas = 11
+    NumJugadores = 2  # Todo lo que empiece con mayúsculas se considera constante
+    CasillaCarcel = 3 # Preguntar de todas formas
+    NumCasillas = 11
     
     def initialize(nombres)
       jugadores = []
@@ -46,9 +46,7 @@ module Civitas
       precioEdificar = 200
       factorRev = 1.2
       nombre = "Calle "
-
-      # Y: 
-      
+     
       #t1 = TituloPropiedad.new(nombre + i, alquiler*i, factorRev, hipotecaBase*i, precioCompra*i, precioEdificar*i++)
       #t2 = TituloPropiedad.new(nombre + i, alquiler*i, factorRev, hipotecaBase*i, precioCompra*i, precioEdificar*i++)
       #t3 = TituloPropiedad.new(nombre + i, alquiler*i, factorRev, hipotecaBase*i, precioCompra*i, precioEdificar*i++)
@@ -62,7 +60,7 @@ module Civitas
       Cantidad_impuesto 50
       impuesto = Casilla.new(Cantidad_impuesto, "Impuesto")
       
-      for i in 1..@@NumCasillas
+      for i in 1..NumCasillas
         case i
         
         when 1
@@ -134,10 +132,98 @@ module Civitas
     end
     
     def pasarTurno
-      indiceJugadorActual = (indiceJugadorActual+1) % @numJugadores
+      indiceJugadorActual = (indiceJugadorActual+1) % NumJugadores
     end    
     
+    def siguientePaso
+      jugadorActual = @jugadores.at(@indiceJugadorActual)
+      operacion = @gestorEstados.operacionesPermitidas(jugadorActual)
+      
+      if operacion == Civitas::OperacionesJuego::PASAR_TURNO
+        pasarTurno()
+        siguientePasoCompletado(operacion)
+      elsif operacion == Civitas::OperacionesJuego::AVANZAR
+        avanzarJugador()
+        siguientePasoCompletado(operacion)
+      end
+    end
     
+    def siguientePasoCompletado(operacion)
+      @estado = @gestionEstados.siguienteEstado(getJugadorActual(), @estado, operacion)
+    end
+    
+    def construirCasa(ip)
+      getJugadorActual().construirCasa(ip)
+    end
+    
+    def construirHotel(ip)
+      getJugadorActual().construirHotel(ip)
+    end
+    
+    def vender(ip)
+      getJugadorActual().vender(ip)
+    end
+    
+    def comprar
+      jugadorActual = @jugadores.at(@indiceJugadorActual)
+      casilla =@tablero.getCasilla(jugadorActual.getNumCasillaActual())
+      titulo = casilla.getTituloPropiedad()
+      jugadorActual.comprar(titulo)
+    end
+    
+    def hipotecar(ip)
+      getJugadorActual().hipotecar(ip)
+    end
+    
+    def cancelarHipoteca(ip)
+      getJugadorActual().cancelarHipoteca(ip)
+    end
+    
+    def salirCarcelPagando
+      getJugadorActual().salirCarcelPagando()
+    end
+    
+    def salirCarcelTirando
+      getJugadorActual().salirCarcelTirando()
+    end
+    
+    def finalDelJuego
+      finaljuego = false
+      
+      for i in 0..NumJugadores and !finaljuego
+        finaljuego = @jugadores.at(i).enBancarrota()
+      end
+      
+      finaljuego
+    end
+    
+    def compareTo(jugador)
+      # completar
+    end
+    
+    def ranking
+      playersrank = []
+      
+      jugadores_aux = @jugadores  # no sé cómo copiar un array
+      
+      for i in 0..NumJugadores
+        max = jugadores_aux.at(0)
+        pos = 0
+        
+        for j in 1..jugadores_aux.size
+          if max.compareTo(jugadores_aux.at(j) < 0)
+            max = jugadores_aux.at(j)
+            pos = j
+          end
+        end
+          
+        playersrank.push(max)
+        jugadores_aux.delete_at(pos)
+        
+      end
+      
+      return playersrank # Se puede poner return pa que quede más claro, no es un error
+    end
     
     
   end

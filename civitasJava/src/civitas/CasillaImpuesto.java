fc84f076de086964fb40094ab83659ac7248eaed
ygonz <ygonz@correo.ugr.es>
2020-12-02 11:27:12 +0100

    Prueba de rama

---
+++ civitasJava/src/civitas/CasillaImpuesto.java
@@ -0,0 +1,14 @@
+/**
+ * @file CasillaImpuesto.java
+ * @author Yesenia González Dávila
+ * @author Esther García Gallego
+ * @brief Casilla de tipo impuesto. Subclase de Casilla.
+ * @note Grupo B.3
+*/
+
+package civitas;
+
+
+public class CasillaImpuesto {
+    
+}
